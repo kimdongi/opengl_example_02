5764047466e50a3f7871bc6b1b643c2ef1f17e9e
kimdongi <kkm0538@naver.com>
2021-05-10 22:34:09 +0900

    여섯번째 커밋

--- src/common.h
+++ src/common.h
@@ -15,8 +15,9 @@
 class klassName; \
 using klassName ## UPtr = std::unique_ptr<klassName>; \
 using klassName ## Ptr = std::shared_ptr<klassName>; \
-using klassName ## WPtr = std::weak_ptr<klassName>;
+using klassName ## WPtr = std::weak_ptr<klassName>; 
 
 std::optional<std::string> LoadTextFile(const std::string& filename);
+glm::vec3 GetAttenuationCoeff(float distance);
 
 #endif // __COMMON_H__
